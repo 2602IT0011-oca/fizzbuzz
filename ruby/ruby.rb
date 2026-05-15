c680be4efe0834498479d0ca7b9b1146c860aa35
2602IT0011-oca <oca2602it0011@edu.oca.ac.jp>
2026-05-15 07:10:32 +0000

    2026/5/15

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -58,14 +58,138 @@
 # alphabet = alphabet.pred
 # puts alphabet
 
-# for integers you can use "pred" function
+# for integers you can use "pred" method
 # num = 5
 # num = num.pred
 # puts num
 
-def add(x, y)
-    puts x
-  x + y  # 最後に評価された値が戻り値になる
-end
+# def add(x, y)
+#     puts x
+#   x + y  # 最後に評価された値が戻り値になる
+# end
 
-puts add(3, 5)  #=> 8
+# puts add(3, 5)  #=> 8
+
+# str = "hello"
+# puts str.reverse
+# puts str
+# puts str. reverse!
+
+# puts str
+
+# puts str.upcase
+# puts str.downcase
+
+# str = "apple,banana,orange"
+# arr = str.split(",")
+# puts arr.inspect  # 結果 : ["apple", "banana", "orange"]
+
+# # 区切り文字を指定しないと、空白で分割される
+# str = "hello world"
+# arr = str.split
+# puts arr.inspect  # 結果 : ["hello", "world"]
+# puts arr
+
+
+# def sum(a, b)
+#     return a + b
+# end
+
+# puts sum(3, 4)
+
+# # 1
+# a = 5
+# b = 10
+# puts a + b
+
+# # 2
+# num = 1
+# puts num % 2 == 0
+# # if num % 2 === 0
+# #     puts true
+# # else
+# #     puts false
+# # end
+
+# # 3
+# str1 = "Hello "
+# str2 = "World!"
+# puts str1 << str2
+
+# # 4
+# a = 3
+# b = 2
+# # if a > b
+# #     puts true
+# # else
+# #     puts false
+# # end
+# puts a > b
+
+# # 5
+# num = 1
+# if num % 2 == 0
+#     puts "Even"
+# else
+#     puts "Odd"
+# end
+
+# # 6
+# a = 1
+# b = 2
+
+# if a > b
+#     puts "aはbより大きい"
+# elsif b > a
+#     puts "bはaより大きい"
+# else
+#     puts "aとbは同じ"
+# end
+
+# # 7
+# (1..10).each {|n|
+# puts n
+# }
+# # num = 1
+
+# # while num <= 10
+# #   puts num
+# #   num += 1
+# # end
+
+# # 8
+# arr = [1, 2, 3]
+
+# arr.each {|n|
+#     puts n
+# }
+
+# # 9
+# def add(a, b)
+#     return a + b
+# end
+
+# puts add(1, 2)
+
+# 10
+# my codes
+# input = [1, 2, 3]
+
+# def calculate_average(arr)
+#     total = 0
+#     arr.each { |i|
+#         total += i
+#     }
+#     return total / arr.length
+# end
+
+# puts calculate_average(input)
+
+# def calculate_average(arr)
+#   total = arr.sum
+#   average = total / arr.length
+#   return average
+# end
+
+# result = calculate_average([100, 200, 300, 400, 500])
+# puts result  # 結果 : 300
